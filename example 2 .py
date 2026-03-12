9c01d933b6b0177002f4130cc75a2b5523dbc662
Sushmithpoojary <birwazz0718@gmail.com>
2026-03-13 00:37:18 +0530

    Add files via upload
    learn swap of two variables

---
+++ example 2 .py
@@ -0,0 +1,10 @@
+#Swap Two Variables:
+# Write a Python program that swaps the values of two variables with and without using a third variable.
+a = 5
+b = 10
+
+# swapping
+a, b = b, a
+
+print("a =", a)
+print("b =", b)
