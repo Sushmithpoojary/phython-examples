7712bc5b8fe80b4e7e050a32f5daa51be51b17d6
Sushmithpoojary <birwazz0718@gmail.com>
2026-03-13 23:04:24 +0530

    Add files via upload
    day 2 learn input and output in python and comments in python

---
+++ 3A1.py
@@ -0,0 +1,7 @@
+print("love") #output
+boy_name =input("Boy Name: ")
+boy_age = int(input("Boy Age: "))
+girl_name =input("Girl Name: ")
+girl_age = int(input("Girl Age: "))
+age_diff = abs (boy_age - girl_age)
+print(f"{boy_name} loves {girl_name} . Age Diffrence is  {age_diff}")
