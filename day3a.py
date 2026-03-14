75945ebf7447aba4264b99817dd85db6e295568e
Sushmithpoojary <birwazz0718@gmail.com>
2026-03-15 01:21:19 +0530

    Add files via upload
    Day 3: learn string manipulation, String methods, accessing string characters, slicing strings

---
+++ day3a.py
@@ -0,0 +1,15 @@
+#Simple Greeting Program: Write a Python program that asks the user for their name and age,
+# then prints a personalized greeting message. Use both the + operator and f-strings for output.
+
+# Ask the user for their name
+name = input("Enter your name: ")
+
+# Ask the user for their age
+age = input("Enter your age: ")
+
+# Greeting using + operator
+print("Hello " + name + ", you are " + age + " years old.")
+
+# Greeting using f-string
+print(f"Nice to meet you, {name}! You are {age} years old.")
+
