0672d5b6fb3d7d5dd5da88fca8d2e369ebc7979e
Sushmithpoojary <birwazz0718@gmail.com>
2026-03-15 23:12:36 +0530

    Add files via upload

---
+++ day 3.py
@@ -0,0 +1,9 @@
+"""Character Counter: Write a Python program that:
+
+Asks the user for a string.
+Prints how many characters are in the string, excluding spaces."""
+
+
+
+N = "python is high intermediate language "
+print(len(N))
