cfa3f578616c9d1ff72ee03d01b78bf0333297a8
Sushmithpoojary <birwazz0718@gmail.com>
2026-03-15 23:17:54 +0530

    Add files via upload

---
+++ day 3 eg4.py
@@ -0,0 +1,11 @@
+"""Escape Sequence Practice: Write a Python program that uses escape sequences to print the following output"""
+
+
+
+
+N  = "my name \n  is sushmith"
+print (N)
+N  = "my name \t  is sushmith"
+print (N)
+N  = "my name \\ is sushmith"
+print (N)
