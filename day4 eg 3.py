191833eac11583ec027433b568bfa4f2ab4ffaf8
Sushmithpoojary <birwazz0718@gmail.com>
2026-03-16 22:54:22 +0530

    Add files via upload

---
+++ day4 eg 3.py
@@ -0,0 +1,8 @@
+"""Membership Operator Exercise: Write a Python program that:
+Takes a string as input from the user.
+Checks if the letter 'a' is in the string (using in).
+Checks if the string doesn't contain the word "Python" (using not in)."""
+
+N = input("Enter a string : ")
+print("a" in N )
+print("Python" not in N)
