30dbee226957c34e026cc09eabd8d48d9e8b941f
Sushmithpoojary <birwazz0718@gmail.com>
2026-03-16 22:54:52 +0530

    Add files via upload

---
+++ day 4 eg4.py
@@ -0,0 +1,12 @@
+""""Bitwise Operator Task: Given two integers, write a Python program that:
+Prints the result of a & b, a | b, and a ^ b.
+Shifts the bits of a two positions to the left (a << 2).
+Shifts the bits of b one position to the right (b >> 1)."""
+
+a = 10
+b = 3
+print(a &b )
+print(a|b)
+print(a^b)
+print(a << 2)
+print(a >> 1)
