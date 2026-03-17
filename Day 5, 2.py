75d51ff8fa06056beb6d0068879d9b46ac45e921
Sushmithpoojary <birwazz0718@gmail.com>
2026-03-18 00:03:51 +0530

    Add files via upload

---
+++ Day 5, 2.py
@@ -0,0 +1,9 @@
+#Reverse and Sort a List: Create a list of numbers and:
+#Sort it in descending ordert.
+#Reverse the sorted list and print it.
+
+list = [1,13,64,78,36,3,6]
+list.sort()
+print(list)
+list.reverse()
+print(list)
