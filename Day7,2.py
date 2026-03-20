8f1167084688734e23e11b6e72a768cad47e3e23
Sushmithpoojary <birwazz0718@gmail.com>
2026-03-20 22:42:17 +0530

    Add files via upload
    📘 Day 7 – Python Dictionaries
    
    Learned how dictionaries store data as key-value pairs 🔑, making data access simple and fast.
    Practiced adding ➕, updating 🔄, deleting ❌ data, and using keys() & values() 📊 methods.

---
+++ Day7,2.py
@@ -0,0 +1,18 @@
+"""Nested Dictionary Practice (Simple for now):
+
+Create a dictionary to store details of two of your friends, including their names, favorite subject, and favorite food.
+Access and print the favorite food of one friend."""
+
+freind1 ={
+    "name":"sumanth",
+    "favorite subject":"Math",
+    "favorite food":"Pizza"
+}
+
+freind2 ={
+    "name":"suneeth",
+    "favorite subject":"Science",
+    "favorite food":"Biryani"
+}
+
+print(freind1["favorite food"])
