2eac55f37fe8d8f21963f8736e56044eb8cca6ec
Sushmithpoojary <birwazz0718@gmail.com>
2026-03-25 21:45:35 +0530

    Add files via upload

---
+++ eg3.py
@@ -0,0 +1,7 @@
+#Write a Python program to take a string from the user and print it in reverse.
+text = input("Enter a string: ")
+
+# Reverse the string
+reversed_text = text[::-1]
+
+print("Reversed string:", reversed_text)
