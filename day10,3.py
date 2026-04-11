8bc422f416560015010269dc28d5b103ef895de3
Sushmithpoojary <birwazz0718@gmail.com>
2026-04-11 16:02:30 +0530

    Add files via upload

---
+++ day10,3.py
@@ -0,0 +1,7 @@
+"""Print Your Name Letter by Letter:
+
+Write a program that takes your name as input and prints each letter of your name using a for loop."""
+
+name = "sushmith"
+for letter in name:
+    print(letter)
