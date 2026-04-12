801ff0958c0a0b847c7a6b2ff8a49c6afa6c0123
Sushmithpoojary <birwazz0718@gmail.com>
2026-04-12 23:32:39 +0530

    Add files via upload

---
+++ day11,3.py
@@ -0,0 +1,7 @@
+"""List of Squares:
+
+Create a list of numbers from 1 to 10. Use list comprehension to generate a list of their squares."""
+l=[x for x in range(1,11)]
+print(l)
+dl=[x**2 for x in l]
+print(dl)
