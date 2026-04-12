348d60c89ff7db27646c5194b83ebeb81fa82e85
Sushmithpoojary <birwazz0718@gmail.com>
2026-04-12 23:31:27 +0530

    Add files via upload
    ## 🚀 Day 11 – List Comparison
    
    Today I learned how to compare lists in Python.
    
    * Check if two lists are the same
    * Compare lists even if order is different
    * Find common and different elements

---
+++ day11,1.py
@@ -0,0 +1,9 @@
+"""List Manipulation:
+
+Create a list of Kannada foods. Use list comprehension to create a new list where each food name is in uppercase."""
+
+food = ["dosa","idli","puri","vada"]
+d1=[item.upper() for item in food]
+print(d1)
+
+    
