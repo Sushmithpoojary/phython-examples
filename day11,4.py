e62c81271ee2b1dffc824f571b9368e58ca7a3f1
Sushmithpoojary <birwazz0718@gmail.com>
2026-04-12 23:35:33 +0530

    Add files via upload

---
+++ day11,4.py
@@ -0,0 +1,13 @@
+"""Student Data Task:
+
+Create a list of 3 dictionaries, where each dictionary contains the name, age, and marks of a student. 
+Loop through the list and print each student's information."""
+student_list = [
+    {"name": "sumanth", "age": 22, "marks": 99},
+    {"name": "suneeth", "age": 20, "marks": 97},
+    {"name": "sushmith", "age": 18, "marks": 88}
+]
+
+for student in student_list:
+    print(student)
+                                   
