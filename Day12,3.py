a7ed073cc7a783cbe1812c93c83e6414d371b769
Sushmithpoojary <birwazz0718@gmail.com>
2026-05-11 23:35:02 +0530

    Add files via upload

---
+++ Day12,3.py
@@ -0,0 +1,5 @@
+#Sum Function: Write a function add_numbers(a, b) that returns the sum of two numbers. Call this function with different values.
+def add_numbers(a,b):
+    return a+b
+result =add_numbers(10,20)
+print("The sum is:",result)
