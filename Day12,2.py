417aab78c848d993cd1c6e9afa05e339cc3bca58
Sushmithpoojary <birwazz0718@gmail.com>
2026-05-11 23:34:37 +0530

    Add files via upload

---
+++ Day12,2.py
@@ -0,0 +1,4 @@
+#Parameterized Greet: Write a function greet_user() that takes a name as input and prints a custom greeting.
+def greet_user(name):
+    print(f"Hello,{name} Good morning!")
+greet_user("Anand")
